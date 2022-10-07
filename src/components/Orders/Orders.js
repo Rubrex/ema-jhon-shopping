810dec4ff85ddd2b958b922ebf6cd502fd5f24d3
Rubel Hossain <rubrex96@hotmail.com>
2022-10-08 03:16:13 +0600

    cleanup

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -7,7 +7,7 @@ import ReviewItem from "../ReviewItem/ReviewItem";
 import "./Orders.css";
 
 const Orders = () => {
-  const { products, initialCart } = useLoaderData();
+  const { initialCart } = useLoaderData();
   const [cart, setCart] = useState(initialCart);
 
   // Remove Cart Button Handler
