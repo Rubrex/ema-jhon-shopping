760084424faa8884a1541c8e65eda68c00913f85
Rubel Hossain <rubrex96@hotmail.com>
2022-10-18 16:49:35 +0600

    **Implementing Firebase

--- src/contexts/UserContext.js
+++ src/contexts/UserContext.js
@@ -1,7 +1,15 @@
 import React from "react";
 import { createContext } from "react";
+import { getAuth } from "firebase/auth";
+import app from "../firebase/firease.config";
+import { useState } from "react";
+
+const auth = getAuth(app);
 
 const UserContext = ({ children }) => {
+  // states
+  const [user, setUser] = useState(null);
+
   const AuthContext = createContext();
 
   const authInfo = {};
